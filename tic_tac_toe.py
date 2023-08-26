cd83665f3a69c0672ecdabd60b99092f6f06ccd5
Maidul <u1703016@student.cuet.ac.bd>
2023-08-26 14:21:55 +0600

    [feat] prompt the human player until a valid input is provided

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -17,6 +17,9 @@ of the winning combinations)
 9 spaces are occupied)
 
 """
+# --------------------------------------------------
+# create initial grid
+# --------------------------------------------------
 
 def create_grid():
     init_grid = """
@@ -30,4 +33,49 @@ def create_grid():
 """
 
     return init_grid
+
+# --------------------------------------------------
+# prompt the human player until a valid input is provided
+# --------------------------------------------------
+
+def prompt_human_player(invalid=False):
+    if invalid:
+        print("Invalid input. Try again.")
+
+    input_from_user = input(
+        "Please choose a position between (1-9) and be sure to choose an empty space: \t"
+    )
+    return input_from_user
+
+# check if user input is valid
+# invalid if 
+# 1) not a number 
+# 2) the input is not a valid key to the played positions dict
+# 3) the position is not empty
+
+
+def is_input_valid(user_input):
+    global played_positions
+    try:
+        user_input = int(user_input)
+    except:
+        return False
+
+    if (played_positions.get(user_input) is False) and (
+        played_positions.get(user_input) is not None
+    ):
+        return user_input
+    else:
+        return False
+    
+# take input from the player
+# prompt until a valid input is chosen
+
+def human_player_input():
+    input_from_user = prompt_human_player()
+
+    while is_input_valid(input_from_user) is False:
+        input_from_user = prompt_human_player(True)
+
+    return int(input_from_user)
 
