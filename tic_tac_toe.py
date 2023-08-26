4c432ab5395ac297a50e8194faa10c043c7a76ef
Maidul <u1703016@student.cuet.ac.bd>
2023-08-26 14:18:36 +0600

    [docs] plan what to do

---
+++ tic_tac_toe.py
@@ -0,0 +1,19 @@
+import random
+
+""" what to do?
+
+1. show the welcome screen
+2. the user plays first
+
+3. playing Steps
+
+# see if any one has already won (if not continue if yes then show who has won with what combination)
+# take user input (see if it's a valid input, if yes then place it on the board, show the grid)
+# see if someone has already won (if not continue if yes then show who has won with what combination)
+# computers turn (see if there are any winning combinations and if it can be blocked. 
+if yes place input there otherwise choose a random empty space or if in intelligent mode try to use one 
+of the winning combinations)
+# continue playing until one wins or it's a draw (when no winning combination is matched and also all 
+9 spaces are occupied)
+
+"""
